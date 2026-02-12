6105f81e9204927c71793a8741d4452306909c5e
Ioan Andrasi <ioan.andrasi@zf.com>
2026-02-12 15:10:58 +0200

    Problem: 1672. Richest Customer Wealth

--- src/easy_problems/intro.rs
+++ src/easy_problems/intro.rs
@@ -1,4 +1,4 @@
-
+use std::collections::HashMap;
 
 
 /*
@@ -28,4 +28,41 @@ pub fn sum_one_array(nums: Vec<i32>) -> Vec<i32> {
         index += 1;
     }
     ret_val
+}
+
+/*
+    1672. Richest Customer Wealth
+
+    You are given an m x n integer grid accounts where accounts[i][j] 
+    is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the ​​​​​​​​​​​th​​​​e bank. 
+    Return the wealth that the richest customer has.
+
+    A customer's wealth is the amount of money they have in all their bank accounts. 
+    The richest customer is the customer that has the maximum wealth.
+
+    Example 1:
+
+    Input: accounts = [[1,2,3],[3,2,1]]
+    Output: 6
+    Explanation:
+    1st customer has wealth = 1 + 2 + 3 = 6
+    2nd customer has wealth = 3 + 2 + 1 = 6
+    Both customers are considered the richest with a wealth of 6 each, so return 6.
+
+    T: 12.18
+*/
+
+pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+    let mut max = 0;
+    let mut sum = 0;
+    for i in accounts {
+        for j in i {
+            sum += j;
+        }
+        if max < sum {
+            max = sum;
+        }
+        sum = 0;
+    }
+    max
 }
--- src/easy_problems/mod.rs
+++ src/easy_problems/mod.rs
@@ -1,9 +1,26 @@
 pub mod intro;
 use intro::sum_one_array;
+use intro::maximum_wealth;
 
 pub fn intro_execution_workspace() {
     let nums = vec![1,2,3,4];
     let ret = sum_one_array(nums);
 
     assert_eq!(3, ret[1]);
+
+    let nums = vec!{
+        vec![1,2,3],
+        vec![3,2,1],
+    };
+    let n = maximum_wealth(nums);
+    assert_eq!(n, 6);
+
+    let nums = vec!{
+        vec![1,5],
+        vec![7,3],
+        vec![3,5],
+    };
+
+    let n = maximum_wealth(nums);
+    assert_eq!(n, 10);
 }
